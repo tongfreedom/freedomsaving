83c8c0fa02f8bc3d6dbd41b3101dbcdad0c0d8da
tongfreedom <tongfreedom@gmail.com>
2014-07-28 02:00:53 +0700

    โครงเว็บ

---
+++ application/views/account/account_view.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ระบบวางแผนทางการเงินส่วนบุคคลออนไลน์</title>
+
+	<link rel="stylesheet" type="text/css" href="<?=base_url(); ?>css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="<?=base_url(); ?>jquery-ui/jquery-ui.css" />
+	<link rel="stylesheet" type="text/css" href="<?=base_url(); ?>bootstrap/css/bootstrap.css" />
+	
+	<style type="text/css">
+		
+		@font-face {
+		    font-family: TS_Country;
+		    src: url("<?=base_url(); ?>font/TS-Country-C-NP.ttf");
+		}
+		.row{
+			margin:0px;
+		}
+	</style>
+
+	<script src="<?=base_url(); ?>js/jquery-1.11.1.js"></script>
+	<script src="<?=base_url(); ?>js/jquery-ui.js"></script>
+	<script src="<?=base_url(); ?>bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+	<div id="container-fluid">
+		<div class="row" style="background-color:#3c8dbc;">
+			<div class="col-md-2 text-center" style="background-color:#367fa9;">
+				<h1 style="font-family:TS_Country;color:#fff;">freedomsaving</h1>
+			</div>
+			<div class="col-md-7">
+				dsdd
+			</div>	
+			<div class="col-md-3">
+				user
+			</div>
+		</div>
+		<div class="row" style="background-color:#ddd;">
+			<div class="col-md-2" style="background-color:#ddd;">
+				menu
+			</div>
+			<div class="col-md-10" style="background-color:#fff;min-height:600px;">
+				content
+				<div style="position:absolute;bottom:0px;">
+					footer
+				</div>
+			</div>	
+		</div>
+	</div>
+</body>
+</html>
